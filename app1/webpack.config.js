9ab4156208cbb782e092c9995bfd06009d5e1238
搬砖达人小王git-flow <461864051@qq.com>
2022-08-30 09:43:47 +0800

    feat: add 注释

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -28,11 +28,15 @@ module.exports = {
   //http://localhost:3002/remoteEntry.js
   plugins: [
     new ModuleFederationPlugin({
+      // 唯一ID，用于标记当前服务
       name: "app1",
+      // 提供给其他服务加载的文件
       filename: "remoteEntry.js",
+      // 需要暴露的模块，使用时通过 `${name}/${expose}` 引入（const RemoteSlides = React.lazy(() => import("app1/Slides"));）
       exposes: {
         "./Slides": "./src/Slides",
       },
+      // 引用 app2 的服务
       remotes: {
         app2: "app2@http://localhost:3002/remoteEntry.js",
       },
